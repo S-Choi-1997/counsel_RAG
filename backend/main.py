401871ca42a14bcc4a1eadceef82a3d2e3d19cda
최승호 <148317140+S-Choi-1997@users.noreply.github.com>
2025-03-03 16:48:45 +0900

    Dockerfile gunicorn change3

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 import os
-import uvicorn
 from fastapi import FastAPI
 from .auth import router as auth_router
 
@@ -10,3 +9,8 @@ app.include_router(auth_router, prefix="/auth", tags=["auth"])
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI on Cloud Run!"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))  # Cloud Run에서 제공하는 PORT 사용
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
